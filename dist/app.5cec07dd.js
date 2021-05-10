0826347d37c113bc4123c5b4d2bdc82e7075ed1f
thepaj <pavla.jureckova@gmail.com>
2021-05-10 16:07:26 +0200

    colors added

--- dist/app.5cec07dd.js
+++ dist/app.5cec07dd.js
@@ -89597,7 +89597,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "34617" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "37353" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
